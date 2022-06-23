abf272c88eb13d02c8aa076fd103b95255249feb
Koima-Sam <sam17koima@gmail.com>
2022-06-23 21:41:17 +0300

    get disease name and facts, then populate the table

--- assets/js/index.js
+++ assets/js/index.js
@@ -12,5 +12,14 @@ function loadDiseases(){
     })
 }
 function renderDisease(item){
-    
+    let div = document.querySelector('#disease-info')
+    let tr = document.createElement('tr')
+    let name = document.createElement('td')
+    name.textContent =item.name
+    let facts = document.createElement('td')
+    facts.textContent =item.facts
+    let date = document.createElement('td')
+    date.textContent = item.data_updated_at
+    tr.append(name, facts, date)
+    div.append(tr)
 }
